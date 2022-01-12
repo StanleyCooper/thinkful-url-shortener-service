8ff79fcfc521d8d4c53019ff4908b4116c6fed1d
Stanley Cooper <scooperlu@gmail.com>
2022-01-12 18:04:51 -0500

    preliminary error handlers

--- src/app.js
+++ src/app.js
@@ -1,4 +1,16 @@
 const express = require("express");
 const app = express();
 
+// Not found handler
+app.use((request, response, next) => {
+    next({ status: 404, message: `Not found: ${request.originalUrl}` });
+});
+  
+// Error handler
+app.use((error, request, response, next) => {
+    console.error(error);
+    const { status = 500, message = "Something went wrong!" } = error;
+    response.status(status).json({ error: message });
+});
+
 module.exports = app;
